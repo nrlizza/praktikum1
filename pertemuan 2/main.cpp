1f468c3ff38df80b7b0982bfd1404b89968286ae
nrlizza <146154455+nrlizza@users.noreply.github.com>
2023-09-27 10:34:37 +0700

    Add files via upload

---
+++ pertemuan 2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+int main () {
+  int tugas, uts, uas;
+  tugas= 70;
+  uas= 80;
+  uts= 95;
+
+  float rata_rata;
+  rata_rata = (tugas, uas, uts)/3;
+
+  cout<< rata_rata << endl;
+}
